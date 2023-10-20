4f792063bea509b6146425e035fb5c0658adc776
Emil1711 <ahm.emil2001@yandex.ru>
2023-10-20 11:58:32 +0300

    привел статистику и напиал отчет

--- 2laba/loop/loop/loop.cpp
+++ 2laba/loop/loop/loop.cpp
@@ -16,218 +16,218 @@ const int t = 10;
 int k = 10;
 int n = 15;
 
-std::vector<int> alpha_to(n);
-std::vector<int> index_of(n);
-std::vector<int> recd(n);
-std::vector<int> s(n - k + 1);
-std::vector<std::vector<int>> elp(n - k + 2, std::vector<int>(n - k));
-std::vector<int> d(n - k + 2);
-std::vector<int> l(n - k + 2);
-std::vector<int> u_lu(n - k + 2);
-std::vector<int> root(t);
-std::vector<int> loc(t);
-std::vector<int> z(t + 1);
-std::vector<int> err(n);
-std::vector<int> reg(t + 1);
-
-
-
-void decode_rs()
-{
-	int i, j, u, q;
-	std::vector<int> s(n - k + 1);
-	std::vector<std::vector<int>> elp(n - k + 2, std::vector<int>(n - k));
-	std::vector<int> d(n - k + 2);
-	std::vector<int> l(n - k + 2);
-	std::vector<int> u_lu(n - k + 2);
-	int count = 0, syn_error = 0;
-	std::vector<int> root(t), loc(t), z(t + 1), err(n), reg(t + 1);
-
-
-	for (i = 0; i < n; i++)
-		recd[i] = index_of[recd[i]];
-
-	for (i = 1; i <= n - k; i++)
-	{
-		s[i] = 0;
-		for (j = 0; j < n; j++)
-			if (recd[j] != -1)
-				s[i] ^= alpha_to[(recd[j] + i * j) % n];
-		if (s[i] != 0)
-			syn_error = 1;
-		s[i] = index_of[s[i]];
-	}
-
-	if (syn_error)
-		d[0] = 0;
-	d[1] = s[1];
-	elp[0][0] = 0;
-	elp[1][0] = 1;
-	for (i = 1; i < n - k; i++)
-	{
-		elp[0][i] = -1;
-		elp[1][i] = 0;
-	}
-	l[0] = 0; l[1] = 0; u_lu[0] = -1; u_lu[1] = 0; u = 0;
-	do
-	{
-		u++;
-		if (d[u] == -1)
-		{
-			l[u + 1] = l[u];
-			for (i = 0; i <= l[u]; i++)
-			{
-				elp[u + 1][i] = elp[u][i];
-				elp[u][i] = index_of[elp[u][i]];
-			}
-		}
-		else
-		{
-
-			q = u - 1;
-			while ((d[q] == -1) && (q > 0))
-				q--;
-
-			if (q > 0)
-			{
-				j = q;
-				do
-				{
-					j--;
-					if ((d[j] != -1) && (u_lu[q] < u_lu[j]))
-						q = j;
-				} while (j > 0);
-			};
-
-			if (l[u] > l[q] + u - q)
-				l[u + 1] = l[u];
-			else
-				l[u + 1] = l[q] + u - q;
-			// формируем новый elp(x)
-			for (i = 0; i < n - k; i++)
-				elp[u + 1][i] = 0;
-			for (i = 0; i <= l[q]; i++)
-				if (elp[q][i] != -1)
-					elp[u + 1][i + u - q] = alpha_to[(d[u] + n - d[q] + elp[q][i]) % n];
-			for (i = 0; i <= l[u]; i++)
-			{
-				elp[u + 1][i] ^= elp[u][i];
-
-				elp[u][i] = index_of[elp[u][i]];
-			}
-		}
-		u_lu[u + 1] = u - l[u + 1];
-		(u < n - k);
-		{
-			if (s[u + 1] != -1)
-				d[u + 1] = alpha_to[s[u + 1]];
-			else
-				d[u + 1] = 0;
-			for (i = 1; i <= l[u + 1]; i++)
-				if ((s[u + 1 - i] != -1) && (elp[u + 1][i] != 0))
-					d[u + 1] ^= alpha_to[(s[u + 1 - i] + index_of[elp[u + 1][i]]) % n];
-
-			d[u + 1] = index_of[d[u + 1]];
-		}
-	} while ((u < n - k) && (l[u + 1] <= t));
-
-	u++;
-	if (l[u] <= t)
-	{
-		for (i = 0; i <= l[u]; i++)
-			elp[u][i] = index_of[elp[u][i]];
-
-		for (i = 1; i <= l[u]; i++)
-			reg[i] = elp[u][i];
-		count = 0;
-		for (i = 1; i <= n; i++)
-		{
-			q = 1;
-			for (j = 1; j <= l[u]; j++)
-				if (reg[j] != -1)
-				{
-					reg[j] = (reg[j] + j) % n;
-					q ^= alpha_to[reg[j]];
-				}
-			if (!q)
-			{
-				root[count] = i;
-				loc[count] = n - i;
-				count++;
-			}
-		}
-		if (count == l[u])
-		{
-			for (i = 1; i <= l[u]; i++)
-			{
-				if ((s[i] != -1) && (elp[u][i] != -1))
-					z[i] = alpha_to[s[i]] ^ alpha_to[elp[u][i]];
-				else
-					if ((s[i] != -1) && (elp[u][i] == -1))
-						z[i] = alpha_to[s[i]];
-					else
-						if ((s[i] == -1) && (elp[u][i] != -1))
-							z[i] = alpha_to[elp[u][i]];
-						else
-							z[i] = 0;
-				for (j = 1; j < i; j++)
-					if ((s[j] != -1) && (elp[u][i - j] != -1))
-						z[i] ^= alpha_to[(elp[u][i - j] + s[j]) % n];
-
-				z[i] = index_of[z[i]];
-			}
-
-			for (i = 0; i < n; i++)
-			{
-				err[i] = 0;
-				if (recd[i] != -1)
-					recd[i] = alpha_to[recd[i]];
-				else
-					recd[i] = 0;
-			}
-
-			for (i = 0; i < l[u]; i++)
-			{
-				err[loc[i]] = 1;
-				for (j = 1; j <= l[u]; j++)
-					if (z[j] != -1)
-						err[loc[i]] ^= alpha_to[(z[j] + j * root[i]) % n];
-				if (err[loc[i]] != 0)
-				{
-					err[loc[i]] = index_of[err[loc[i]]];
-					q = 0;
-					for (j = 0; j < l[u]; j++)
-						if (j != i)
-							q += index_of[1 ^ alpha_to[(loc[j] + root[i]) % n]];
-					q = q % n;
-					err[loc[i]] = alpha_to[(err[loc[i]] - q + n) % n];
-
-					recd[loc[i]] ^= err[loc[i]];
-				}
-			}
-		}
-		else
-		{
-
-			for (i = 0; i < n; i++)
-				if (recd[i] != -1) recd[i] = alpha_to[recd[i]];
-				else
-					recd[i] = 0;
-		}
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			if (recd[i] != -1)
-				recd[i] = alpha_to[recd[i]];
-			else
-				recd[i] = 0;
-		}
-	}
-
-	
-}
+//std::vector<int> alpha_to(n);
+//std::vector<int> index_of(n);
+//std::vector<int> recd(n);
+//std::vector<int> s(n - k + 1);
+//std::vector<std::vector<int>> elp(n - k + 2, std::vector<int>(n - k));
+//std::vector<int> d(n - k + 2);
+//std::vector<int> l(n - k + 2);
+//std::vector<int> u_lu(n - k + 2);
+//std::vector<int> root(t);
+//std::vector<int> loc(t);
+//std::vector<int> z(t + 1);
+//std::vector<int> err(n);
+//std::vector<int> reg(t + 1);
+//
+//
+//
+//void decode_rs()
+//{
+//	int i, j, u, q;
+//	std::vector<int> s(n - k + 1);
+//	std::vector<std::vector<int>> elp(n - k + 2, std::vector<int>(n - k));
+//	std::vector<int> d(n - k + 2);
+//	std::vector<int> l(n - k + 2);
+//	std::vector<int> u_lu(n - k + 2);
+//	int count = 0, syn_error = 0;
+//	std::vector<int> root(t), loc(t), z(t + 1), err(n), reg(t + 1);
+//
+//
+//	for (i = 0; i < n; i++)
+//		recd[i] = index_of[recd[i]];
+//
+//	for (i = 1; i <= n - k; i++)
+//	{
+//		s[i] = 0;
+//		for (j = 0; j < n; j++)
+//			if (recd[j] != -1)
+//				s[i] ^= alpha_to[(recd[j] + i * j) % n];
+//		if (s[i] != 0)
+//			syn_error = 1;
+//		s[i] = index_of[s[i]];
+//	}
+//
+//	if (syn_error)
+//		d[0] = 0;
+//	d[1] = s[1];
+//	elp[0][0] = 0;
+//	elp[1][0] = 1;
+//	for (i = 1; i < n - k; i++)
+//	{
+//		elp[0][i] = -1;
+//		elp[1][i] = 0;
+//	}
+//	l[0] = 0; l[1] = 0; u_lu[0] = -1; u_lu[1] = 0; u = 0;
+//	do
+//	{
+//		u++;
+//		if (d[u] == -1)
+//		{
+//			l[u + 1] = l[u];
+//			for (i = 0; i <= l[u]; i++)
+//			{
+//				elp[u + 1][i] = elp[u][i];
+//				elp[u][i] = index_of[elp[u][i]];
+//			}
+//		}
+//		else
+//		{
+//
+//			q = u - 1;
+//			while ((d[q] == -1) && (q > 0))
+//				q--;
+//
+//			if (q > 0)
+//			{
+//				j = q;
+//				do
+//				{
+//					j--;
+//					if ((d[j] != -1) && (u_lu[q] < u_lu[j]))
+//						q = j;
+//				} while (j > 0);
+//			};
+//
+//			if (l[u] > l[q] + u - q)
+//				l[u + 1] = l[u];
+//			else
+//				l[u + 1] = l[q] + u - q;
+//			// формируем новый elp(x)
+//			for (i = 0; i < n - k; i++)
+//				elp[u + 1][i] = 0;
+//			for (i = 0; i <= l[q]; i++)
+//				if (elp[q][i] != -1)
+//					elp[u + 1][i + u - q] = alpha_to[(d[u] + n - d[q] + elp[q][i]) % n];
+//			for (i = 0; i <= l[u]; i++)
+//			{
+//				elp[u + 1][i] ^= elp[u][i];
+//
+//				elp[u][i] = index_of[elp[u][i]];
+//			}
+//		}
+//		u_lu[u + 1] = u - l[u + 1];
+//		(u < n - k);
+//		{
+//			if (s[u + 1] != -1)
+//				d[u + 1] = alpha_to[s[u + 1]];
+//			else
+//				d[u + 1] = 0;
+//			for (i = 1; i <= l[u + 1]; i++)
+//				if ((s[u + 1 - i] != -1) && (elp[u + 1][i] != 0))
+//					d[u + 1] ^= alpha_to[(s[u + 1 - i] + index_of[elp[u + 1][i]]) % n];
+//
+//			d[u + 1] = index_of[d[u + 1]];
+//		}
+//	} while ((u < n - k) && (l[u + 1] <= t));
+//
+//	u++;
+//	if (l[u] <= t)
+//	{
+//		for (i = 0; i <= l[u]; i++)
+//			elp[u][i] = index_of[elp[u][i]];
+//
+//		for (i = 1; i <= l[u]; i++)
+//			reg[i] = elp[u][i];
+//		count = 0;
+//		for (i = 1; i <= n; i++)
+//		{
+//			q = 1;
+//			for (j = 1; j <= l[u]; j++)
+//				if (reg[j] != -1)
+//				{
+//					reg[j] = (reg[j] + j) % n;
+//					q ^= alpha_to[reg[j]];
+//				}
+//			if (!q)
+//			{
+//				root[count] = i;
+//				loc[count] = n - i;
+//				count++;
+//			}
+//		}
+//		if (count == l[u])
+//		{
+//			for (i = 1; i <= l[u]; i++)
+//			{
+//				if ((s[i] != -1) && (elp[u][i] != -1))
+//					z[i] = alpha_to[s[i]] ^ alpha_to[elp[u][i]];
+//				else
+//					if ((s[i] != -1) && (elp[u][i] == -1))
+//						z[i] = alpha_to[s[i]];
+//					else
+//						if ((s[i] == -1) && (elp[u][i] != -1))
+//							z[i] = alpha_to[elp[u][i]];
+//						else
+//							z[i] = 0;
+//				for (j = 1; j < i; j++)
+//					if ((s[j] != -1) && (elp[u][i - j] != -1))
+//						z[i] ^= alpha_to[(elp[u][i - j] + s[j]) % n];
+//
+//				z[i] = index_of[z[i]];
+//			}
+//
+//			for (i = 0; i < n; i++)
+//			{
+//				err[i] = 0;
+//				if (recd[i] != -1)
+//					recd[i] = alpha_to[recd[i]];
+//				else
+//					recd[i] = 0;
+//			}
+//
+//			for (i = 0; i < l[u]; i++)
+//			{
+//				err[loc[i]] = 1;
+//				for (j = 1; j <= l[u]; j++)
+//					if (z[j] != -1)
+//						err[loc[i]] ^= alpha_to[(z[j] + j * root[i]) % n];
+//				if (err[loc[i]] != 0)
+//				{
+//					err[loc[i]] = index_of[err[loc[i]]];
+//					q = 0;
+//					for (j = 0; j < l[u]; j++)
+//						if (j != i)
+//							q += index_of[1 ^ alpha_to[(loc[j] + root[i]) % n]];
+//					q = q % n;
+//					err[loc[i]] = alpha_to[(err[loc[i]] - q + n) % n];
+//
+//					recd[loc[i]] ^= err[loc[i]];
+//				}
+//			}
+//		}
+//		else
+//		{
+//
+//			for (i = 0; i < n; i++)
+//				if (recd[i] != -1) recd[i] = alpha_to[recd[i]];
+//				else
+//					recd[i] = 0;
+//		}
+//	}
+//	else
+//	{
+//		for (i = 0; i < n; i++)
+//		{
+//			if (recd[i] != -1)
+//				recd[i] = alpha_to[recd[i]];
+//			else
+//				recd[i] = 0;
+//		}
+//	}
+//
+//	
+//}
 
 
 
@@ -333,7 +333,7 @@ int main() {
 			printf("Corrupt!!!\n");
 
 
-			decode_rs();
+			/*decode_rs();*/
 
 
 
